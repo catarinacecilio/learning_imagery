12ed332b2993ac4e3045ca4514c262c8abc75d9c
Arthur Barretto Leite de Barros <65037537+Arthur-barretto@users.noreply.github.com>
2023-04-01 10:05:28 -0300

    Add files via upload

---
+++ code/backend/fast_api/app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import Response
+
+import numpy as np
+import cv2
+import io
+from backend.penguin_rec.penguin_rec import annotate_img
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'status':'ok'}
+
+@app.post('/upload_image')
+async def receive_image(img: UploadFile=File(...)):
+    ### Receiving and decoding the image
+    contents = await img.read()
+
+    nparr = np.fromstring(contents, np.uint8)
+    cv2_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # type(cv2_img) => numpy.ndarray
+
+    ### Do cool stuff with your image.... For example face detection
+    annotated_img = annotate_img(cv2_img)
+
+    ### Encoding and responding with the image
+    im = cv2.imencode('.png', annotated_img)[1] # extension depends on which format is sent from Streamlit
+    return Response(content=im.tobytes(), media_type="image/png")
